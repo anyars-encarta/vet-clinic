09b4accbf570731720ee3b96a21dd199039e9a58
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-09 18:41:03 +0000

    Update species and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,12 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
+SELECT * FROM animals;
