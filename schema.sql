281ee8c361b84ed3fdc495ae8202e2b93ffe0cf9
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-09 10:23:34 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    id INT NOT NULL,
-    name varchar(100) NULL,
-    date_of_birth date NULL,
-    escape_attempts INT NULL,
-	neutered boolean NULL,
-	weight_kg decimal,
-	PRIMARY KEY (id)
+    name varchar(100)
 );
