c253dcc15f27df0514a84b9b8ad12612ed71d194
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-09 09:25:34 +0000

    Add Schema to create Database and animals Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT NOT NULL,
+    name varchar(100) NULL,
+    date_of_birth date NULL,
+    escape_attempts INT NULL,
+	neutered boolean NULL,
+	weight_kg decimal,
+	PRIMARY KEY (id)
 );
