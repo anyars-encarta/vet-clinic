47db92f2acdc8b5fe0c06c77c43330ff3e3ac9ca
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-10 13:02:58 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -14,3 +14,31 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species varchar(100) NULL;
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100) NULL,
+    age INT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NULL,
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species,
+
+ADD species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE,
+
+ADD owner_id INT,
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
