09428fcc6102d84a4b5541c5287f3eb3822bbf95
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-10 08:36:56 +0000

    Remove unnecesary white spaces

--- queries.sql
+++ queries.sql
@@ -9,63 +9,41 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
 
-
 BEGIN;
 
 UPDATE animals
 SET species = 'unspecified';
-
 ROLLBACK;
 
 SELECT * FROM animals;
 
-
-
 BEGIN;
-
 UPDATE animals SET species = 'digimon' WHERE name like '%mon';
-
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-
 COMMIT;
 
 SELECT * FROM animals;
 
-
 BEGIN;
-
 DELETE FROM animals;
-
 ROLLBACK;
 
 SELECT * FROM animals;
 
-
 BEGIN;
-
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-
 SAVEPOINT Point1;
-
 UPDATE animals SET weight_kg = weight_kg * -1;
-
 ROLLBACK TO Point1;
-
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-
 COMMIT;
 
 SELECT * FROM animals;
-
 
 SELECT COUNT(*) FROM animals;
-
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
-
 SELECT AVG(weight_kg) FROM animals;
-
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
-
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 SELECT species, AVG(escape_attempts) AS average_escape_attempts
