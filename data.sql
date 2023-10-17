2a7c95a53f0c67063a5df75bd934bbf418d0fb96
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-10-17 11:26:10 +0200

    comments for data

--- data.sql
+++ data.sql
@@ -75,7 +75,8 @@ INSERT INTO visits (animals_id, vets_id, visit_date) VALUES
 (65, 3, '2020-05-24'),
 (65, 1, '2021-01-11');
 
-
+-- inserting dummy data into visits to chck perfomance
 INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
+-- inserting dummy data into owners to check perfomance
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
